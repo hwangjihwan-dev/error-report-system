3c1c88e6c4a6ac898367727fd659ab17b3e60301
hwangjihwan <hwang19970727@gmail.com>
2026-05-24 16:50:26 +0900

    스프링 부트 프로젝트 연결

---
+++ src/main/java/com/hwang/errorreport/ErrorReportSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hwang.errorreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorReportSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErrorReportSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hwang/errorreport/ErrorReportSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwang.errorreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorReportSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
