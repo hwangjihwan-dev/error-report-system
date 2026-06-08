8f6e0e4b9d841d0cd83d7ef807b84402fa4ca05f
hwangjihwan <hwang19970727@gmail.com>
2026-06-09 08:45:32 +0900

    관리자 오류신고 상세 조회 및 답변 등록 기능 구현

--- src/main/java/com/hwang/errorreport/domain/report/ReportStatus.java
+++ src/main/java/com/hwang/errorreport/domain/report/ReportStatus.java
@@ -1,7 +1,17 @@
 package com.hwang.errorreport.domain.report;
 
+import lombok.Getter;
+
+@Getter
 public enum ReportStatus {
-    RECEIVED, //접수
-    IN_PROGRESS, //처리중
-    COMPLETED //완료
+
+    RECEIVED("접수"), //접수
+    IN_PROGRESS("처리중"), //처리중
+    COMPLETED("완료"); //완료
+
+    private final String description;
+
+    ReportStatus(String description){
+        this.description = description;
+    }
 }
